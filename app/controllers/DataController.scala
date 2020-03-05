8e6d68065baf9d09765ec2bbafba894da6218c17
Faizan Khalid <faizan@datumbrain.com>
2020-03-05 14:53:23 +0500

    [NO-OP] Formatted Code

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -4,39 +4,14 @@ import java.io.FileInputStream
 
 import javax.inject._
 import play.api.libs.json._
-import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
 
 import play.api.mvc._
 
 import scala.collection.JavaConverters._
-/*
-case class responseData(time_of_crawl: String,
-                        page_url: String,
-                        inbound_links: Int,
-                        outbound_links: Int,
-                        content: JsValue)
 
-object responseData {
-  implicit val responseDataWrites: Writes[responseData] = (
-    (JsPath \ "time_to_crawl").write[String] and
-      (JsPath \ "page_url").write[String] and
-      (JsPath \ "inbound_links").write[Int] and
-      (JsPath \ "outbound_links").write[Int] and
-      (JsPath \ "content").write[JsValue]
-  )(unlift(responseData.unapply))
 
-	implicit val responseDataReads: Reads[responseData] = (
-	  (JsPath \ "time_to_crawl").read[String] and
-		(JsPath \ "page_url").read[String] and
-		(JsPath \ "inbound_links").read[Int] and
-		(JsPath \ "outbound_links").read[Int] and
-		(JsPath \ "content").read[JsValue]
-	  )(responseData.apply _)
-}
- */
 @Singleton
-class DataController @Inject()(val controllerComponents: ControllerComponents)
+class DataController @Inject() (val controllerComponents: ControllerComponents)
     extends BaseController {
 
   import java.io.File
@@ -49,13 +24,14 @@ class DataController @Inject()(val controllerComponents: ControllerComponents)
     {
       var result: JsArray = Json.arr()
       for (file <- recursiveListFiles(new java.io.File("./output/"))
-           if file.getName endsWith ".json") {
+           if file.getName.endsWith(".json")) {
         val stream = new FileInputStream(file)
-        val json = try {
-          Json.parse(stream)
-        } finally {
-          stream.close()
-        }
+        val json =
+          try {
+            Json.parse(stream)
+          } finally {
+            stream.close()
+          }
         result = result.append(json)
       }
       Ok(result)
@@ -69,9 +45,14 @@ class DataController @Inject()(val controllerComponents: ControllerComponents)
       val inbound_links = json.get("inbound_links")
       var result: JsArray = Json.arr()
       for (file <- recursiveListFiles(new java.io.File("./output/"))
-           if file.getName endsWith ".json") {
+           if file.getName.endsWith(".json")) {
         val stream = new FileInputStream(file)
-        val json = try { Json.parse(stream) } finally { stream.close() }
+        val json =
+          try {
+            Json.parse(stream)
+          } finally {
+            stream.close()
+          }
         if ((json \ "page_url").get == page_url && (json \ "inbound_links").get == inbound_links)
           result = result.append(json)
       }
